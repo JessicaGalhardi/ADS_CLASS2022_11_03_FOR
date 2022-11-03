c735f02e9d7c7db2b2117ff3bb2e5034cb7c2595
JessicaGalhardi <104734490+JessicaGalhardi@users.noreply.github.com>
2022-11-03 18:21:30 +0100

    Adicionar arquivos de projeto.

---
+++ ADS_CLASS2022_11_03_FOR/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        try
+        {
+            var x = 10;
+
+            for (int i = 0; i < x; i++) 
+            {
+                Console.WriteLine("i = " + i) ;
+
+                if (i == 8)
+                {
+                    break;
+                }
+            }
+
+            Console.ReadKey();
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error while running the application" + ex);
+
+        }
+    }
+}
